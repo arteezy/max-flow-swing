d1a4649f45c132dc337ca5712a08d4c7da268bc1
yeasayer <yeasayer@yeasayer-pc>
2010-10-06 21:13:35 +0700

    boolean flags, instead of int

--- src/karp/Main.java
+++ src/karp/Main.java
@@ -7,26 +7,25 @@ public class Main {
 
     public static void dijkstra(int graph[][]) {
         int d[] = new int[graph.length];
-        int dC[] = new int[graph.length];
+        boolean flags[] = new boolean[graph.length];
         int p[] = new int[graph.length];
         for (int i = 0; i < graph.length; i++) {
             d[i] = 100;
-            dC[i] = 100;
+            flags[i] = true;
             p[i] = -1;
         }
         d[0] = 0;
-        dC[0] = 0;
 
-        int i = 0, min = 200, pos = 0; //You can change the min to 1000 to make it the largest number
+        int i = 0, min = 200, pos = 0;
         while (i < graph.length) {
             //extract minimum
-            for (int j = 0; j < dC.length; j++) {
-                if (min > d[j] && dC[j] != -1) {
+            for (int j = 0; j < flags.length; j++) {
+                if (min > d[j] && flags[j] != false) {
                     min = d[j];
                     pos = j;
                 }
             }
-            dC[pos] = -1;
+            flags[pos] = false;
 
             //relax
             for (int j = 0; j < graph.length; j++) {
@@ -49,12 +48,14 @@ public class Main {
         System.out.println();
     }
 
+
     public static void path(int[] p, int to) {
         if (p[to] != -1) {
             System.out.print(p[to] + " ");
             path(p, p[to]);
         }
     }
+
 
 
   public static int[][] fileRead() {
