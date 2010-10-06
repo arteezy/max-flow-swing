4d231a265839defd2606a0504eee33b9f9d42fe9
yeasayer <yeasayer@yeasayer-pc>
2010-10-06 21:20:47 +0700

    replaced 100 by Integer.MAX_VALUE

--- src/karp/Main.java
+++ src/karp/Main.java
@@ -6,19 +6,20 @@ import java.util.*;
 public class Main {
 
     public static void dijkstra(int graph[][]) {
-        int d[] = new int[graph.length];
-        boolean flags[] = new boolean[graph.length];
+        
+        int d[] = new int[graph.length];        
         int p[] = new int[graph.length];
-        for (int i = 0; i < graph.length; i++) {
-            d[i] = 100;
-            flags[i] = true;
-            p[i] = -1;
-        }
+        boolean flags[] = new boolean[graph.length];
+
+        Arrays.fill(d, Integer.MAX_VALUE);
+        Arrays.fill(flags, true);
+        Arrays.fill(p, -1);
+
         d[0] = 0;
 
-        int i = 0, min = 200, pos = 0;
+        int i = 0, min = Integer.MAX_VALUE, pos = 0;
         while (i < graph.length) {
-            //extract minimum
+            //minimum
             for (int j = 0; j < flags.length; j++) {
                 if (min > d[j] && flags[j] != false) {
                     min = d[j];
@@ -91,7 +92,6 @@ public class Main {
 
   public static void main(String[] args) {
       int[][] turbik = fileRead();
-      System.out.println(turbik.length);
       int s = 0;
       int t = 2;
       dijkstra (turbik);
