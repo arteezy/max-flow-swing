2913b8ad5658955564709ea0048460f0dd2f71b3
yeasayer <yeasayer@yeasayer-pc>
2010-10-06 20:29:21 +0700

    Removed bfs, added Dijkstra

--- src/karp/Main.java
+++ src/karp/Main.java
@@ -5,20 +5,57 @@ import java.util.*;
 
 public class Main {
 
-//  public static Path getPath(Edge[] p, int s, int t) {
-//    Path path = new Path();
-//    path.f = Integer.MAX_VALUE;
-//    for (; t != s; t = p[t].s) {
-//      path.edges.add(p[t]);
-//      path.f = Math.min(path.f, p[t].cap - p[t].f);
-//      System.out.println(s + " " + t + " " + path.f);
-//    }
-//    return path;
-//  }
+    public static void dijkstra(int graph[][]) {
+        int d[] = new int[graph.length];
+        int dC[] = new int[graph.length];
+        int p[] = new int[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            d[i] = 100;
+            dC[i] = 100;
+            p[i] = -1;
+        }
+        d[0] = 0;
+        dC[0] = 0;
 
-  public static void bfs(int[][] turbik, int s, int t) {
-      
-  }
+        int i = 0, min = 200, pos = 0; //You can change the min to 1000 to make it the largest number
+        while (i < graph.length) {
+            //extract minimum
+            for (int j = 0; j < dC.length; j++) {
+                if (min > d[j] && dC[j] != -1) {
+                    min = d[j];
+                    pos = j;
+                }
+            }
+            dC[pos] = -1;
+
+            //relax
+            for (int j = 0; j < graph.length; j++) {
+                if (d[j] > graph[pos][j] + d[pos]) {
+                    d[j] = graph[pos][j] + d[pos];
+                    p[j] = pos;
+                }
+            }
+            i++;
+            min = 200;
+        }
+
+        System.out.println();
+        System.out.println("1 2 3 4 5 6");
+        for (int j = 0; j < p.length; j++) System.out.print(p[j] + " ");
+        System.out.println();
+        path(p, 2);
+        System.out.println();
+        for (int j = 0; j < d.length; j++) System.out.print(d[j] + " ");
+        System.out.println();
+    }
+
+    public static void path(int[] p, int to) {
+        if (p[to] != -1) {
+            System.out.print(p[to] + " ");
+            path(p, p[to]);
+        }
+    }
+
 
   public static int[][] fileRead() {
 
@@ -54,9 +91,9 @@ public class Main {
   public static void main(String[] args) {
       int[][] turbik = fileRead();
       System.out.println(turbik.length);
-      s = 0;
-      t = 2;
-      bfs (turbik, s, t);
+      int s = 0;
+      int t = 2;
+      dijkstra (turbik);
 
   }
 }
